8fc69dd94bfb368b8695278faa4f2498a71de0f3
Sivaprakash1606 <sivasnprakash3@gmail.com>
2024-06-17 16:24:42 +0530

    excapsulation using getter and setter

---
+++ Encapsulation.py
@@ -0,0 +1,25 @@
+#Encapsulation:
+
+#Binding of Variables and Methods
+
+#Getters and Setters
+class Car:
+    def __init__(self,car_name,no_of_wheels,no_of_airbags,mileage):
+        self.car_name=car_name
+        self.__no_of_wheels=no_of_wheels
+        self.no_of_airbags=no_of_airbags
+        self.mileage=mileage
+
+    #getter
+    def get_no_of_wheels(self):
+        return self.__no_of_wheels
+
+    #setter
+    def set_no_of_wheels(self,no_of_wheels):
+        self.__no_of_wheels=no_of_wheels
+
+
+car1=Car("Maruthi",4,4,25.0)
+print(car1.get_no_of_wheels())
+car1.set_no_of_wheels(8)
+print(car1.get_no_of_wheels())
