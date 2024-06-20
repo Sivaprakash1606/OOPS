ff101a84094d2bfa2ac73759c611eb36fd55ce5f
Sivaprakash1606 <sivasnprakash3@gmail.com>
2024-06-20 14:22:44 +0530

    super

---
+++ Super.py
@@ -0,0 +1,12 @@
+class Person():
+    def __init__(self,name):
+        self.name=name
+class Student(Person):
+    def __init__(self,name,grade):
+        super().__init__(name)
+        self.grade=grade
+    def display(self):
+        print(self.name,self.grade)
+
+s1=Student("Siva","A")
+s1.display()
